131e7c5a7fc8ef55ea99d3d7c5d08618e30b3c8b
dfmr96 <dfmr96@gmail.com>
2022-10-01 18:16:45 -0300

    -Player lose points from object that disappear if not clicked

--- OctosoftTechTask/Assets/Scripts/GameManager.cs
+++ OctosoftTechTask/Assets/Scripts/GameManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager sharedInstance;
     public int totalPoints = 0;
+
+    public float counter = 0;
+
 
     private void Awake()
     {
@@ -14,5 +18,10 @@ public class GameManager : MonoBehaviour
         {
             sharedInstance = this;
         }
+    }
+
+    private void Update()
+    {
+        counter = Time.fixedTime;
     }
 }
--- OctosoftTechTask/Assets/Scripts/ObjectStats.cs
+++ OctosoftTechTask/Assets/Scripts/ObjectStats.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -11,9 +12,27 @@ public class ObjectStats : MonoBehaviour, IPointerClickHandler
     public int health = 0;
     [SerializeField] int secondsToBeDestroyed = 5;
 
+
+    private void Start()
+    {
+        StartCoroutine(LifeTimer());
+    }
+    IEnumerator LifeTimer()
+    {
+        yield return new WaitForSeconds(secondsToBeDestroyed);
+        LossPointsOnDisappear(pointsLost);
+        Destroy(gameObject);
+    }
+
+    
     private void GrantPoints(int points)
     {
         GameManager.sharedInstance.totalPoints += points;
+    }
+
+    void LossPointsOnDisappear(int points)
+    {
+        GameManager.sharedInstance.totalPoints -= points;
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -24,18 +43,8 @@ public class ObjectStats : MonoBehaviour, IPointerClickHandler
 
         if(health <= 0)
         {
+            GrantPoints(pointsGranted);
             Destroy(gameObject);
         }
-    }
-
-    private void OnDestroy()
-    {
-        GrantPoints(pointsGranted);
-    }
-
-    IEnumerator LifeTimer()
-    {
-        yield return new WaitForSeconds(secondsToBeDestroyed);
-        Destroy(gameObject);
     }
 }
